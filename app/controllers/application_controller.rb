e792c3f8faa860423943c3617e82a552446c60c3
Marc Price <dpl@DPL-ADMISSIONS-A1398s-MacBook-Pro.local>
2019-12-02 20:34:40 -0700

    Done with Divise authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  
+  before_action :configure_permitted_perameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+    # def accout_params
+    #   params.require(:user)(:sign_up).permit(:email, :password, :first_name, :last_name)
+    # end
+  
+    def configure_permitted_perameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    end
+
 end
